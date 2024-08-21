178d7b5939664280d1f4d43f27a45a3310abfd3c
floppydiskette <floppydisk@hyprcat.net>
2024-08-21 21:13:55 +0100

    I have massively fucked up

--- src/main/java/net/hypr/doki/Config.java
+++ src/main/java/net/hypr/doki/Config.java
@@ -4,13 +4,13 @@ import com.google.gson.Gson;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
 
 //You can add more fields in this class, if your input json matches the structure
 //You will need a valid config.json in the package com.freya02.bot for this to work
 public class Config {
     @SuppressWarnings("unused") private String token;
-    @SuppressWarnings("unused") private String prefix;
-    @SuppressWarnings("unused") private DBConfig mariadb;
+    @SuppressWarnings("unused") private DBConfig dbConfig;
 
     /**
      * Returns the configuration object for this bot
@@ -19,18 +19,18 @@ public class Config {
      * @throws IOException if the config JSON could not be read
      */
     public static Config readConfig() throws IOException {
+        System.out.println(Path.of("config.json"));
         try (InputStream in=Thread.currentThread().getContextClassLoader().getResourceAsStream("config.json")) {
             assert in != null;
             Reader reader = new InputStreamReader(in, StandardCharsets.UTF_8);
             return new Gson().fromJson(reader, Config.class);
         } catch (IOException e) {
             throw new IOException("""
-					config.json was not found in the root folder (of the project), did you forget to create it ?
+					config.json was not found in the root folder (of the project), did you forget to put it ?
 					Example structure:
 
 					{
-						"token": "[your_bot_token_here]",
-						"prefix": "!"
+						"token": "[your_bot_token_here]"
 					}
 					""", e);
         }
@@ -39,19 +39,17 @@ public class Config {
     public String getToken() {
         return token;
     }
-    public String getPrefix() {
-        return prefix;
-    }
 
     public DBConfig getDbConfig() {
-        return mariadb;
+        return dbConfig;
     }
 
     public static class DBConfig {
-        @SuppressWarnings("unused") private String host, user, password, database;
+        @SuppressWarnings("unused") private String serverName, user, password, dbName;
+        @SuppressWarnings("unused") private int portNumber;
 
-        public String getHost() {
-            return host;
+        public String getServerName() {
+            return serverName;
         }
 
         public String getUser() {
@@ -62,8 +60,12 @@ public class Config {
             return password;
         }
 
-        public String getDatabase() {
-            return database;
+        public String getDbName() {
+            return dbName;
+        }
+
+        public int getPortNumber() {
+            return portNumber;
         }
     }
 }
