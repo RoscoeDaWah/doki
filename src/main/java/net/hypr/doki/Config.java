bf7aafad0cf07a21f4f1e6c16b0759c1322fa6dc
floppydiskette <floppydisk@hyprcat.net>
2024-10-25 00:38:05 +0100

    Axe levelling

--- src/main/java/net/hypr/doki/Config.java
+++ src/main/java/net/hypr/doki/Config.java
@@ -13,7 +13,6 @@ public class Config {
     @SuppressWarnings("unused") private String token;
     @SuppressWarnings("unused") private String prefix;
     @SuppressWarnings("unused") private DBConfig mariadb;
-    @SuppressWarnings("unused") private boolean levelling = false;
 
     /**
      * Returns the configuration object for this bot
@@ -37,7 +36,6 @@ public class Config {
         return token;
     }
     public String getPrefix() { return prefix; }
-    public boolean getLevelling() { return levelling; }
 
     public DBConfig getDbConfig() {
         return mariadb;
--- src/main/java/net/hypr/doki/Doki.java
+++ src/main/java/net/hypr/doki/Doki.java
@@ -6,8 +6,6 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.requests.GatewayIntent;
-import net.hypr.doki.listeners.LevellingListener;
-import org.apache.commons.dbcp2.BasicDataSource;
 import org.slf4j.Logger;
 
 import java.io.IOException;
@@ -16,7 +14,6 @@ public class Doki {
     private static final Logger log = Logging.getLogger();
     private static JDA jda;
     private static Config config;
-    private static final BasicDataSource dataSource = new BasicDataSource();
 
     private Doki(JDA jda, Config config) {
         Doki.jda = jda;
@@ -27,7 +24,6 @@ public class Doki {
         return jda;
     }
     public static String getPrefix() { return config.getPrefix(); }
-    public static BasicDataSource getDataSource() { return dataSource; }
 
     public static void start() throws IOException, InterruptedException {
         config = Config.readConfig();
@@ -37,19 +33,6 @@ public class Doki {
                 .enableIntents(GatewayIntent.MESSAGE_CONTENT)
                 .build()
                 .awaitReady();
-
-        // Connect to the DB
-        if (config.getLevelling()) {
-            Config.DBConfig dbConfig = config.getDbConfig();
-            dataSource.setDriverClassName("org.mariadb.jdbc.Driver");
-            dataSource.setUrl("jdbc:mariadb://" + dbConfig.getHost() + ":" + dbConfig.getPortNumber() + "/" + dbConfig.getDatabase());
-            dataSource.setUsername(dbConfig.getUser());
-            dataSource.setPassword(dbConfig.getPassword());
-            dataSource.setMaxTotal(10);
-            dataSource.setMaxIdle(5);
-            dataSource.setMinIdle(2);
-            dataSource.setInitialSize(10);
-        }
 
         // Print some information about the bot
         log.info("Bot connected as {}", jda.getSelfUser().getAsTag());
@@ -68,11 +51,6 @@ public class Doki {
             CommandsBuilder.newBuilder(437970062922612737L)
                     .textCommandBuilder(textCommandsBuilder -> textCommandsBuilder.addPrefix(getPrefix()))
                     .build(jda, "net.hypr.doki.commands"); //Registering listeners is taken care of by the lib
-            if (config.getLevelling()) {
-                jda.addEventListener(new LevellingListener());
-            } else {
-                log.info("Levelling is disabled");
-            }
         } catch (Exception e) {
             log.error("Failed to start the bot", e);
             System.exit(-1);
